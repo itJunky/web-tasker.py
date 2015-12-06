b0d8c8dbcb6fbb4a85fe9218750484cc793630b1
itjunky <itjunky@jeff.ws>
2015-12-07 01:52:08 +0300

    Add some actions for task

--- web_tasker/views.py
+++ web_tasker/views.py
@@ -18,9 +18,38 @@ def index():
   return render_template('index.html', title='Hi', user_from=user_from, user=user) 
 
 @app.route("/task")
-@app.route("/task/<number>")
-def task(number=None):
-  return render_template('task.html', tittle='Задачи', number=number, user=user)
+@app.route("/task/<action>", methods=['GET', 'POST'])
+def task(action='list'):
+  if action == 'list':
+    if user == None:
+      return render_template('task.html', tittle='Задачи', user=user)
+    cur = db.session.execute("SELECT id FROM user WHERE nickname='{}'".format(user))
+    user_id = cur.fetchone()[0]
+    cur = db.session.execute("SELECT id, taskname, timestamp FROM task WHERE user_id='{}'".format(user_id))
+    tasks = cur.fetchall()
+    return render_template('task.html', tittle='Задачи', user=user, task_list=tasks)
+
+  elif action=='create':
+    if request.method == 'POST':
+      # Getting user id
+      cur = db.session.execute("SELECT id FROM user WHERE nickname='{}'".format(user))
+      user_id = cur.fetchone()[0]
+      task_row = models.Task(user_id=user_id, taskname=request.form['taskname'], body=request.form['taskbody'], timestamp=datetime.datetime.now())
+      db.session.add(task_row)
+      db.session.commit()
+    return render_template('task_create.html', tittle='Задачи', user=user)
+
+  elif action=='view':
+    task_id = request.args.get('id')
+    cur = db.session.execute("SELECT taskname,body,timestamp FROM task WHERE id='{}'".format(task_id))
+    #return str(cur.fetchone())
+    return render_template('task_view.html', tittle='Задачи', user=user, task_expl=cur.fetchone())
+
+
+  elif action=='modify':
+    pass
+
+  return render_template('task.html', tittle='Задачи', user=user)
 
 @app.route("/profile")
 def profile():
