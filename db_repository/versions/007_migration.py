fd116b7a2f95794bec46e4dd891ffc37e03882e5
itjunky <itjunky@jeff.ws>
2015-12-07 01:53:25 +0300

    Add some db migrates

---
+++ db_repository/versions/007_migration.py
@@ -0,0 +1,37 @@
+from sqlalchemy import *
+from migrate import *
+
+
+from migrate.changeset import schema
+pre_meta = MetaData()
+post_meta = MetaData()
+post = Table('post', pre_meta,
+    Column('id', INTEGER, primary_key=True, nullable=False),
+    Column('body', VARCHAR(length=140)),
+    Column('timestamp', DATETIME),
+    Column('user_id', INTEGER),
+)
+
+task = Table('task', post_meta,
+    Column('id', Integer, primary_key=True, nullable=False),
+    Column('body', String(length=140)),
+    Column('timestamp', DateTime),
+    Column('user_id', Integer),
+)
+
+
+def upgrade(migrate_engine):
+    # Upgrade operations go here. Don't create your own engine; bind
+    # migrate_engine to your metadata
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    pre_meta.tables['post'].drop()
+    post_meta.tables['task'].create()
+
+
+def downgrade(migrate_engine):
+    # Operations to reverse the above upgrade go here.
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    pre_meta.tables['post'].create()
+    post_meta.tables['task'].drop()
