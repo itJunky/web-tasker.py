3fab8a00a859743e2b7ea314ce9393ea52c4352c
itjunky <itjunky@jeff.ws>
2016-01-06 16:01:15 +0300

    More verbosity for debugging

--- runserver.py
+++ runserver.py
@@ -1,7 +1,17 @@
 from web_tasker import app
 import logging
+#logging.basicConfig()
+
 log = logging.getLogger()
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.ERROR)
+
+sql_log = logging.getLogger('sqlalchemy.engine')
+sql_log.setLevel(logging.DEBUG)
+
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
