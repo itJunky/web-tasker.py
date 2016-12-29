ec150cb77b7044ebe6530015f7a6f0d9ddbd07b8
itjunky <itjunky@jeff.ws>
2016-12-29 13:32:24 +0300

    Fix merge conflict

--- db_create.py
+++ db_create.py
@@ -8,17 +8,15 @@ db = SQLAlchemy(app)
 
 
 import os.path
-<<<<<<< HEAD
 
 with app.app_context():
         # Extensions like Flask-SQLAlchemy now know what the "current" app
         # is while within this block. Therefore, you can now run........
         db.create_all()
 # db.create_all()
-=======
 #db.create_all(app)
-db.create_all()
->>>>>>> c3bffdc8fe3d5f8670e507141a8c494f0372b842
+#db.create_all()
+
 if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
     api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
