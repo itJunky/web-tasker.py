678ea9b2ab51c29e06dbb0c20710dbde50324db9
pinepike <post@nojs.ga>
2018-01-10 04:38:42 +0300

    solve issue #6
      add `comment form` validation to length less than 6 letters

--- web_tasker/static/handler.js
+++ web_tasker/static/handler.js
@@ -7,3 +7,34 @@ window.onload = function () {
       document.getElementById("dinamic-form").style.display="none";
   }
 }
+
+//////
+// add `comment form` validation to length less than 6 letters
+//////
+$(document).ready(function () {
+  const COMMENT_LENGTH = 6;
+
+  let commentTextSelector = 'form[action="/comment_to_task"] [name="commenttext"]';
+  let commentFormSelector = 'form[action="/comment_to_task"]';
+
+  $('body').on('focus', commentTextSelector, function (e) {
+    let $commentText = $(this);
+    let $helpBlock = $commentText.parent().find('.help-block');
+    $helpBlock.addClass('invisible');
+    $commentText.parent().removeClass('has-error');
+  });
+
+  $('body').on('submit', commentFormSelector, function(e) {
+
+    let $commentText = $('[name="commenttext"]');
+    let $helpBlock = $commentText.parent().find('.help-block');
+
+    if ($commentText.val().length < COMMENT_LENGTH) {
+      e.preventDefault();
+      $commentText.parent().addClass('has-error');
+      $helpBlock.removeClass('invisible');
+      return false;
+    }
+  });
+
+});
