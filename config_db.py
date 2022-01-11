7eebcbfa1da488c10f30becfe5886d5eb4ad3441
itjunky <itjunky@github.com>
2022-01-11 19:18:26 +0300

    set fixed charset for db connection

--- config_db.py
+++ config_db.py
@@ -5,5 +5,5 @@ SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 SQLITE_OMIT_FOREIGN_KEY = False
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db') + '?charset=utf8mb4'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
